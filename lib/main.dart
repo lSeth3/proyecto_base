6c36108f4c07c6a16118f49f9a5eb138e3fb8c4e
lSeth3 <edwardstev372@gmail.com>
2023-12-07 08:47:46 -0500

    clase 7 de noviembre

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'app/presentation/app.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main()async {
+  await dotenv.load(fileName:"assets/global/.env");
+  await Supabase.initialize(
+    url:dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_KEY']!,
+  );
   runApp(const App());
 }
